dce6118909b09c87415db2091662839127c740a0
Deric Miguel <deric.miguel@gmail.com>
2016-07-03 21:03:55 -0500

    creating setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#git
+echo "git: setting user email"
+git config --global user.email "deric.miguel@gmail.com"
+
+# vim
+echo "vim: linking files and installing vundle"
+ln -sf `pwd`/vim ~/.vim
+ln -sf `pwd`/vim/vimrc ~/.vimrc
+if [ ! -e ~/.vim/bundle/Vundle.vim ]
+then
+   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+# oh-my-zsh
+echo "oh-my-zsh: installing oh-my-zsh"
+if [ ! -e ~/.oh-my-zsh ]
+then
+   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+ln -sf `pwd`/zsh/zshrc ~/.zshrc
